f6de110eac74b63b85502fc5ac2bfcbe79705f6e
vijaykunigiri17 <vijaykunigiri17@gmail.com>
2025-03-13 07:06:03 +0530

    Lets Add

--- NumberList.cpp
+++ NumberList.cpp
@@ -1,7 +1,28 @@
+// 
 #include<bits/stdc++.h>
 using namespace std;
+int func(int i,int j,vector<int>&wt,vector<int>&cp,vector<vector<int>>&dp){
+  if(j==0) return 0;
+  if(i==0){
+    if(cp[i]<=j) return wt[i];
+    return 0;
+  }
+
+  int pick=0;
+  if(cp[i]<=j){
+    pick=wt[i]+func(i-1,j-cp[i],wt,cp,dp);
+  }
+  int np=func(i+1,j,wt,cp,dp);
+  return max(pick,np);
+  
+}
 int main (){
-      cout<<"Printing Numbers From 1 To 100"<<endl;
-      for(int i=1;i<=100;i++) cout<<i<<" ";
+      vector<int>wt={10,40,30,50};
+      vector<int>cp={5,4,2,3};
+      int w=5;
+      vector<vector<int>>dp(cp.size(),vector<int>(w+1,-1));
+      
+      cout<<func(cp.size()-1,w,wt,cp,dp);
+
     return 0;
 }
